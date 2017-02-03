5e4638ae98076321ef838180e5d000e49f43e857
Davide Bontempelli <davide.bonte@gmail.com>
2017-02-03 16:54:34 +0100

    extract negative numbers detector

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -17,11 +17,15 @@ module.exports = class StringCalculator {
       return numbers.filter(number => parseInt(number) < 1000).reduce((total, number) => total += parseInt(number), 0)
     } else {
       if (hasNegativeNumbers(stringNumber)) {
-        return () => { throw new Error(`negatives not allowed: ${stringNumber.match(/-\d*/g).toString()}`) }
+        return () => { throw new Error(`negatives not allowed: ${getNegativeNumbersFromString(stringNumber)}`) }
       }
       return () => { throw new Error('invalid input') }
     }
   }
+}
+
+function getNegativeNumbersFromString(stringNumber){
+  return stringNumber.match(/-\d*/g).toString()
 }
 
 function hasNegativeNumbers(stringNumber){
